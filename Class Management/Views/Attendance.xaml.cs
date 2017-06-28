d0ba4d2a313dc4101075c12bff6bfb063a94c1cd
k-slugger <shubhamghosalkar.sg@gmail.com>
2017-06-28 17:12:20 +0530

    Add blank layout for attendance

---
+++ Class Management/Views/Attendance.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Class_Management.Views
+{
+    /// <summary>
+    /// Interaction logic for Attendance.xaml
+    /// </summary>
+    public partial class Attendance : UserControl
+    {
+        public Attendance()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            Storyboard mystory;
+            mystory = (Storyboard)App.Current.Resources["sb"];
+            mystory.Begin(this);
+        }
+
+        private void closeUC_Click(object sender, RoutedEventArgs e)
+        {
+            (this.Parent as Grid).Children.Remove(this);
+        }
+    }
+}
