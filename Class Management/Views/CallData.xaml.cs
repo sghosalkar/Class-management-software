bfd82a02f896c3f3d43a9005bfe88db8efd24724
k-slugger <shubhamghosalkar.sg@gmail.com>
2017-07-06 17:27:02 +0530

    Add code to import excel data

--- Class Management/Views/CallData.xaml.cs
+++ Class Management/Views/CallData.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,16 +34,128 @@ namespace Class_Management.Views
             InitializeComponent();
         }
 
+        SQLiteConnection conn = new SQLiteConnection(@"Data Source=Database\MainDatabase.db;Version=3;");
+
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
             Storyboard mystory;
             mystory = (Storyboard)App.Current.Resources["sb"];
             mystory.Begin(this);
+            conn.Open();
+            FillDataGrid();
         }
 
         private void closeUC_Click(object sender, RoutedEventArgs e)
         {
             (this.Parent as Grid).Children.Remove(this);
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            conn.Close();
+        }
+
+        private void SaveCallData_Click(object sender, RoutedEventArgs e)
+        {
+            if(!Int64.TryParse(ContactNo.Text, out long result))
+            {
+                MessageBox.Show("Enter proper contact number");
+                return;
+            }
+            try
+            {
+                string sql = "INSERT INTO calldata(name, contact_no) VALUES('" + StudentName.Text + "', '" + ContactNo.Text + "');";
+                SQLiteCommand command = new SQLiteCommand(sql, conn);
+                command.ExecuteNonQuery();
+                command.Dispose();
+                FillDataGrid();
+                MessageBox.Show("Saved");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void FillDataGrid()
+        {
+            try
+            {
+                string sql = "SELECT * FROM calldata;";
+                SQLiteCommand command = new SQLiteCommand(sql, conn);
+                command.ExecuteNonQuery();
+                SQLiteDataAdapter dataAdp = new SQLiteDataAdapter(command);
+                DataTable dt = new DataTable("calldata");
+                dataAdp.Fill(dt);
+                CallDataDataGrid.ItemsSource = dt.DefaultView;
+                dataAdp.Update(dt);
+                command.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ImportExcel_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.DefaultExt = ".xlsx";
+            openfile.Filter = "(.xlsx)|*.xlsx";
+            //openfile.ShowDialog();
+
+            var browsefile = openfile.ShowDialog();
+
+            if (browsefile == true)
+            {
+                string TextFilePath = openfile.FileName;
+
+                Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+                //Static File From Base Path...........
+                //Microsoft.Office.Interop.Excel.Workbook excelBook = excelApp.Workbooks.Open(AppDomain.CurrentDomain.BaseDirectory + "TestExcel.xlsx", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                //Dynamic File Using Uploader...........
+                Microsoft.Office.Interop.Excel.Workbook excelBook = excelApp.Workbooks.Open(TextFilePath, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                Microsoft.Office.Interop.Excel.Worksheet excelSheet = (Microsoft.Office.Interop.Excel.Worksheet)excelBook.Worksheets.get_Item(1); ;
+                Microsoft.Office.Interop.Excel.Range excelRange = excelSheet.UsedRange;
+
+                string strCellData = "";
+                double douCellData;
+                int rowCnt = 0;
+                int colCnt = 0;
+
+                DataTable dt = new DataTable();
+                for (colCnt = 1; colCnt <= excelRange.Columns.Count; colCnt++)
+                {
+                    string strColumn = "";
+                    strColumn = (string)(excelRange.Cells[1, colCnt] as Microsoft.Office.Interop.Excel.Range).Value2;
+                    dt.Columns.Add(strColumn, typeof(string));
+                }
+
+                for (rowCnt = 2; rowCnt <= excelRange.Rows.Count; rowCnt++)
+                {
+                    string strData = "";
+                    for (colCnt = 1; colCnt <= excelRange.Columns.Count; colCnt++)
+                    {
+                        try
+                        {
+                            strCellData = (string)(excelRange.Cells[rowCnt, colCnt] as Microsoft.Office.Interop.Excel.Range).Value2;
+                            strData += strCellData + "|";
+                        }
+                        catch (Exception ex)
+                        {
+                            douCellData = (excelRange.Cells[rowCnt, colCnt] as Microsoft.Office.Interop.Excel.Range).Value2;
+                            strData += douCellData.ToString() + "|";
+                        }
+                    }
+                    strData = strData.Remove(strData.Length - 1, 1);
+                    dt.Rows.Add(strData.Split('|'));
+                }
+
+                CallDataDataGrid.ItemsSource = dt.DefaultView;
+
+                excelBook.Close(true, null, null);
+                excelApp.Quit();
+            }
         }
     }
 }
